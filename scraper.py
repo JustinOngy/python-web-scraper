ee3e4c8f4fe8e8bb277230485b6b7877cd328f99
Justin Ong <justin-ong@live.com.au>
2024-02-29 19:34:06 +1100

    first commit

---
+++ scraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://example.com/'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+title = soup.find('title').text
+
+print('Page Title:', title)
